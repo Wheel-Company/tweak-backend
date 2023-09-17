7cbd3e00fe7fe0087a54aa64079ca81774227ec1
crom <crom.kim@juice.co.kr>
2023-09-18 00:41:32 +0900

    (chore) Adjust allowed hosts

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -3,7 +3,7 @@ from backend.settings.base import *
 DEBUG = True
 # CORS 관련
 
-ALLOWED_HOSTS =["13.125.139.26:8000"]
+ALLOWED_HOSTS =["13.125.139.26"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
